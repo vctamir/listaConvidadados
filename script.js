df52767efdafe511b965149acb7b1ee694caf4f0
Valdemir <vc.tamir@gmail.com>
2020-05-16 13:30:47 -0400

    Aplicação de Styles

--- script.js
+++ script.js
@@ -25,14 +25,22 @@ function atualizarLista() {
 
     for (let i = 0; i < nomesConvidados.length; i++) {
 
-        lista.innerHTML = lista.innerHTML + `<div>
+        lista.innerHTML = lista.innerHTML + `<div class="divRemover">
                            <span> Nome: ${nomesConvidados[i].nome}<br>  
                            Idade: ${nomesConvidados[i].idade} anos <br>
-                           Email: ${nomesConvidados[i].email}</span>     
+                           Email: ${nomesConvidados[i].email}</span>    
+                           <button class="btnRemover" onclick="removeLista(${i})"> 
+                             <img  src="img/buttonRemover.svg">
+                           </button> 
                            </div>`
     }
 
     lista.scrollTop = lista.scrollHeight - lista.clientHeight;
 
 
+}
+
+function removeLista(intPos) {
+    nomesConvidados.splice(intPos, 1);
+    atualizarLista();
 }
